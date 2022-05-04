1a15552165f455106b44ca5f986693017cb94f2f
Rumata-SL <kirr7619@gmail.com>
2022-05-05 01:43:17 +0500

    Added icons

--- src/component/skils/Skil.tsx
+++ src/component/skils/Skil.tsx
@@ -1,13 +1,14 @@
 import React, {FC} from "react";
 import skil from "./Skil.module.css"
-import logo from "./logo.svg"
+
 
 type SkilTypeProps={
     title:string
     description:string
+    logo:string
 }
 
-export const Skil:FC<SkilTypeProps>= ({title,description})=>{
+export const Skil:FC<SkilTypeProps>= ({title,description, logo})=>{
     return(
         <div className={skil.wrapper}>
             <img src={logo} alt="logo" className={skil.logo}/>
--- src/component/skils/Skils.tsx
+++ src/component/skils/Skils.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import skils from "./Skils.module.css"
 import {Skil} from "./Skil";
 import {Title} from "../title/Title";
+import logo from "./icons/logo.svg"
+import html5 from "./icons/html5.svg"
+import css from "./icons/css.svg"
+import js from "./icons/js.svg"
+import git from "./icons/git.svg"
+import jest from "./icons/jest.svg"
 
 export const Skils = () => {
 
@@ -12,12 +18,12 @@ export const Skils = () => {
             <div className={skils.container}>
                 <Title title={"MAIN SKILS"}/>
                 <div className={skils.elements}>
-                    <Skil title={"REACT"} description={description}/>
-                    <Skil title={"HTML"} description={description}/>
-                    <Skil title={"CSS"} description={description}/>
-                    <Skil title={"JS"} description={description}/>
-                    <Skil title={"GIT"} description={description}/>
-                    <Skil title={"JEST"} description={description}/>
+                    <Skil title={"REACT"} description={description} logo={logo}/>
+                    <Skil title={"HTML"} description={description} logo={html5}/>
+                    <Skil title={"CSS"} description={description} logo={css}/>
+                    <Skil title={"JS"} description={description} logo={js}/>
+                    <Skil title={"GIT"} description={description} logo={git}/>
+                    <Skil title={"JEST"} description={description} logo={jest}/>
                 </div>
             </div>
         </div>
