e978e4914788c883f0350cd9a6292d55b6bdcfe6
Rumata-SL <kirr7619@gmail.com>
2022-05-06 01:54:01 +0500

    Added style

--- src/component/contacts/Contacts.tsx
+++ src/component/contacts/Contacts.tsx
@@ -4,7 +4,17 @@ import c from "./Contacts.module.css"
 export const Contacts = () => {
     return (
         <div className={c.wrapper}>
-            <div className={c.container}>Contacts</div>
+            <div className={c.container}><form className={c.form}>
+                <div className={c.inputBlock}>
+                    <input type="text" placeholder="Your Name"/>
+                    <input type="email" placeholder="Your Email"/>
+                </div>
+                <textarea placeholder="Your Message"/>
+                <button className={c.btn}>
+                    Send Message
+                </button>
+            </form>
+                );</div>
         </div>
     );
 }
